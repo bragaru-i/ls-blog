37bd7814d295e48d2b653e987d881fef14bdb1d7
fargusMD <bragaruion@gmail.com>
2020-06-22 21:55:24 +0400

    Updates

---
+++ precache-manifest.425cb06e0040504aea0a3b86208eaeb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3523ef95a50d77c54784ec7a09aa301",
+    "url": "/ls-blog/index.html"
+  },
+  {
+    "revision": "488e411038b7f0a24d12",
+    "url": "/ls-blog/static/css/2.a33842c1.chunk.css"
+  },
+  {
+    "revision": "22d1cb68e6707f880bbd",
+    "url": "/ls-blog/static/css/main.ee716659.chunk.css"
+  },
+  {
+    "revision": "488e411038b7f0a24d12",
+    "url": "/ls-blog/static/js/2.822112b6.chunk.js"
+  },
+  {
+    "revision": "96de6a35e0f03f6cfc5e9aa0dc033382",
+    "url": "/ls-blog/static/js/2.822112b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d1cb68e6707f880bbd",
+    "url": "/ls-blog/static/js/main.dceda7cd.chunk.js"
+  },
+  {
+    "revision": "4d0891e74c7bb2745805",
+    "url": "/ls-blog/static/js/runtime-main.17930a11.js"
+  },
+  {
+    "revision": "8ffc6e2d1e0c928991e854e4ba0218d7",
+    "url": "/ls-blog/static/media/footer.8ffc6e2d.jpg"
+  }
+]);
